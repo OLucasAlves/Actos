f70d909efbd2aeae93163910355018f0a7a7c463
Lucas <Lucas>
2017-05-03 16:20:16 -0300

    Adicionando Slick carousel ao projeto e alterando menu e imagens do site

---
+++ js/parceiro.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function() {
+  
+      $('.autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        });    
+    });
